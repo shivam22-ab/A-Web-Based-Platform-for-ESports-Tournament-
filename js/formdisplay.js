eaba73ca457d1a17dcd3563d14fc4315230b187c
Shivam Ranabhat <shivamranabhat@gmail.com>
2022-07-21 16:29:29 +0545

    Add Playlist

--- js/formdisplay.js
+++ js/formdisplay.js
@@ -7,3 +7,31 @@ $(document).ready(function(){
         $("#log-form").hide();
     });
 });
+// For prevent default and hide password on eye icon click in form
+const forms = document.querySelector(".forms"),
+    pwShowHide = document.querySelectorAll(".eye-icon"),
+    links = document.querySelectorAll(".link");
+
+pwShowHide.forEach(eyeIcon => {
+    eyeIcon.addEventListener("click", () => {
+        let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll(".password");
+
+        pwFields.forEach(password => {
+            if (password.type === "password") {
+                password.type = "text";
+                eyeIcon.classList.replace("bx-hide", "bx-show");
+                return;
+            }
+            password.type = "password";
+            eyeIcon.classList.replace("bx-show", "bx-hide");
+        })
+
+    })
+})
+
+links.forEach(link => {
+    link.addEventListener("click", e => {
+        e.preventDefault(); //preventing form submit
+        forms.classList.toggle("show-signup");
+    })
+})
